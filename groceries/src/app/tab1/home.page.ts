cc7078dd0d190836f08bf082e5ebbc7f1093ed2d
Cara Solomon <solomon.tacara@outlook.com>
2019-08-11 23:36:01 -0500

    added social sharing

--- groceries/src/app/tab1/home.page.ts
+++ groceries/src/app/tab1/home.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ToastController } from '@ionic/angular';
 import { GroceriesServiceService } from '../groceries-service.service';
 import { InputDialogService } from '../input-dialog.service';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @Component({
   selector: 'app-home',
@@ -13,11 +14,30 @@ export class HomePage {
   title = 'Grocery';
 
   // tslint:disable-next-line: max-line-length
-  constructor(public toastCtrl: ToastController, public dataService: GroceriesServiceService, public inputDialogService: InputDialogService) { }
+  constructor(public toastCtrl: ToastController, public dataService: GroceriesServiceService, public inputDialogService: InputDialogService, public socialSharing: SocialSharing) { }
 
   // Load items list
   loadItems() {
     return this.dataService.getItems();
+  }
+  // Share items from list
+  async shareItem(item, index) {
+    console.log('Sharing item - ', item, index);
+    const toast = await this.toastCtrl.create({
+      message: 'Sharing item - ' + index + '...',
+      duration: 3000
+    });
+    toast.present();
+
+    const message = 'Grocery Item - Name: ' +  item.name + ' Quantity: ' + item.quantity;
+    const subject = 'Shared via Groceries App';
+    this.socialSharing.share(message, subject).then(() => {
+      // Success!
+      console.log('Shared successfully');
+    }).catch(() => {
+      // Error!
+      console.error('Share was unsuccesful');
+    });
   }
   // Delete items from list
   async deleteItem(item, index) {
